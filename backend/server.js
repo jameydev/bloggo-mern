63475dfd25f89a4d468de4562e94a058a53ab8ff
jameydev <jybryce4@gmail.com>
2023-08-10 21:59:51 -0500

    errorHandler added. fleshed out server.js more; db

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
-const { connectDB } = require('./config/db');
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/errorMiddleware');
 
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
+// app.use('/api/users', require('./routes/users'));
 // app.use('/api/home', require('./routes/home'));
+
+app.use(errorHandler);
 
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 5000;
