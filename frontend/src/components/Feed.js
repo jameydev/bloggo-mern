1dab8a7a108d487ba4d8ad7f262139adec243ac2
jameydev <jybryce4@gmail.com>
2023-08-12 15:31:00 -0500

    started work on post feed component

---
+++ frontend/src/components/Feed.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function Feed() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await axios.get('/api/posts');
+            setPosts(res.data);
+        };
+
+        fetchPosts();
+    }, []);
+
+    return (
+        <div className="feed">
+            <h1>Feed</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post._id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.content}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
