308db79305dc84fef1faa590ba992c03562af9a8
jameydev <jybryce4@gmail.com>
2023-08-12 12:59:34 -0500

    restarting PC due to issues

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -1,13 +1,17 @@
-const Home = require('../models/Home');
+const Home = require('../models/homeModel');
 
 // @desc    Get home page
 // @route   GET /api/home
 // @access  Public
 const getHome = async (req, res, next) => {
     try {
-        const home = await Home.find();
-        res.status(200).json(home);
+        // const home = await Home.find();
+        res.status(200).json({ message: 'Welcome to the home page!' });
     } catch (err) {
         next(err);
     }
 }
+
+module.exports = {
+    getHome
+};
