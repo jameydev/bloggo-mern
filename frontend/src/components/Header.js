9230eb78d3fa17be06694a310001414680f82cb9
jameydev <jybryce4@gmail.com>
2023-08-12 18:01:59 -0500

    Header update; Navbar component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,11 @@
+import Navbar from "./Navbar/Navbar";
+
 export default function Header(props) {
     return (
         <header>
+            <Navbar />
             <h1 class="txt-center">{props.children}</h1>
+            <hr />
         </header>
     );
 
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar(props) {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Settings</a></li>
+                <li><a href="#">New Post</a></li>
+            </ul>
+        </nav>
+    );
+}
